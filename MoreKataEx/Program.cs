52c749ea625ac70c404185808d559c2220995668
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-05-31 18:24:57 +0200

    Add project files.

---
+++ MoreKataEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreKataEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
